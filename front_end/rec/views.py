5f10074698f25cb638cbe9214aacf98bfeb2e092
Aaron Lee <aaron.k.lee1@gmail.com>
2019-08-08 00:33:20 -0700

    changes to RS function and return False for unchecked checkbox

--- front_end/rec/views.py
+++ front_end/rec/views.py
@@ -10,49 +10,79 @@ def home(request):
     return render(request, 'home.html', {'title': 'New Vehicle Recommendation System'})
 
 
-def knowledge_based_rec(df,
-                        manufacturer=None,
-                        category=None,
-                        price=None,
-                        comfort=None,
-                        driving=None,
-                        interior=None,
-                        tech=None,
-                        utility=None):
+# def knowledge_based_rec(df,manufacturer=None, category=None,price=None,comfort=None,driving=None,interior=None,tech=None,utility=None):
+    # int(False) = 0, int(True) = 1
+    # attributes = {'comfort': int(comfort), 'driving': int(driving), 'interior': int(interior), 
+    #               'tech': int(tech), 'utility':int(utility)}
 
-    if comfort+driving+interior+tech+utility == 1:
-        cu = []
+    # sum = int(comfort) + int(driving) + int(interior) + int(tech) + int(utility)
+    # if 0 == sum:
+    #     weight = 0.2
+    # else:
+    #     weight = 1/sum
+    # # calculated utilty depending on the User's Preference
+    # cu = []
+    # for i, row in df.iterrows():
+    #     calculated_utility = (attributes['comfort']*weight)*row['comfort'] + (attributes['driving']*weight)*row['driving']
+    #     + (attributes['interior']*weight)*row['interior'] + (attributes['tech']*weight)*row['tech'] + (attributes['utility']*weight)*row['utility']
+        
+    #     cu.append(calculated_utility)
+    # df.insert(0, 'Calculated Utility', cu, allow_duplicates=True)
+    # int(False) = 0, int(True) = 1
+def knowledge_based_rec(df,manufacturer=None, category=None,price=None,comfort=None,driving=None,interior=None,tech=None,utility=None):
+    # int(False) = 0, int(True) = 1
+    attributes = {'comfort': int(comfort), 'driving': int(driving), 'interior': int(interior), 
+                  'tech': int(tech), 'utility':int(utility)}
+
+    cu = []
+
+    sum = int(comfort) + int(driving) + int(interior) + int(tech) + int(utility)
+    if 0 == sum:
+        weight = 0.2
         for i, row in df.iterrows():
-            calculated_utility = comfort*row['comfort'] + driving*row['driving']
-            + interior*row['interior'] + tech*row['tech'] + utility*row['utility']
+            calculated_utility = weight*row['comfort'] + weight*row['driving'] + weight*row['interior'] + weight*row['tech'] + weight*row['utility']
             cu.append(calculated_utility)
-        df.insert(0, 'Calculated Utility', cu, allow_duplicates=True)
     else:
-        print(f"Percentages of comfort, driving, interior, tech, and utility need to equal 100%. Try Again.")
+        weight = 1/sum
+        for i, row in df.iterrows():
+            calculated_utility = (attributes['comfort']*weight)*row['comfort'] + (attributes['driving']*weight)*row['driving'] + (attributes['interior']*weight)*row['interior'] + (attributes['tech']*weight)*row['tech'] + (attributes['utility']*weight)*row['utility']
+            cu.append(calculated_utility)
+
+    print(f"Weight: {weight}")
+    print(cu)
+    if 'Calculated Utility' in df.columns:
+        del df['Calculated Utility']
+
+    df.insert(0, 'Calculated Utility', cu, allow_duplicates=True)
+
+
 
 def recommend(request):
+    # Get path and Read CSV FIle with weight of each vehicles
     cwd = os.getcwd()
     csv_name = "post_process_edmund_rating.csv"
-    # DATA_SRC = "C:/Users/Aaron/workspace/cmpe256/car_rec_system/front_end/rec/post_process_edmund_rating.csv"
     DATA_SRC = os.path.join(cwd, 'rec', csv_name)
-    print(f"DATA_SRC: {DATA_SRC}")
     df = pd.read_csv(DATA_SRC)
 
     manufacturer = request.POST['manufacturer']
     veh_type = request.POST['vehicle_type']
 
-    # comfort = request.POST['comfort']
-    # driving = request.GET['driving']
-    # interior = request.GET['interior']
-    # tech = request.GET['tech']
-    # utilitiy = request.GET['utility']
+    price = request.POST.get('price', None)
 
-    knowledge_based_rec(df, manufacturer, veh_type, None, .14, .23, .21, .12, .30)
+    comfort = request.POST.get('comfort', False)
+    driving = request.POST.get('driving', False)
+    interior = request.POST.get('interior', False)
+    tech = request.POST.get('tech', False)
+    utility = request.POST.get('utility', False)
+
+    # knowledge_based_rec(df, manufacturer, veh_type, None, .14, .23, .21, .12, .30)
+    knowledge_based_rec(df, manufacturer, veh_type, None, comfort, driving, interior, tech, utility)
+
     z = df.sort_values('Calculated Utility').tail(10)
     z = z.to_html()
     # z = z.to_html(escaped=False)
 
-    # context = {'data': d}
+    # context = {'data': price}
     # context = {'data': DATA_SRC}
     context = {'data': z}
 
