71d2e680b23228f421dec53f581bdcc34bc7fd3a
Aaron Lee <aaron.k.lee1@gmail.com>
2019-08-09 09:19:58 -0700

    Get calcualted utility for weighted and no weight

--- front_end/rec/views.py
+++ front_end/rec/views.py
@@ -6,29 +6,11 @@ import os
 # Create your views here.
 
 def home(request):
-    # return HttpResponse("<h1>Hello World!</h1>")
-    return render(request, 'home.html', {'title': 'New Vehicle Recommendation System'})
+    """
+    Test === Used to pass the title and render to home.html
+    """
+    return render(request, 'home.html', {'title': 'Vehicle Recommendation System'})
 
-
-# def knowledge_based_rec(df,manufacturer=None, category=None,price=None,comfort=None,driving=None,interior=None,tech=None,utility=None):
-    # int(False) = 0, int(True) = 1
-    # attributes = {'comfort': int(comfort), 'driving': int(driving), 'interior': int(interior),
-    #               'tech': int(tech), 'utility':int(utility)}
-
-    # sum = int(comfort) + int(driving) + int(interior) + int(tech) + int(utility)
-    # if 0 == sum:
-    #     weight = 0.2
-    # else:
-    #     weight = 1/sum
-    # # calculated utilty depending on the User's Preference
-    # cu = []
-    # for i, row in df.iterrows():
-    #     calculated_utility = (attributes['comfort']*weight)*row['comfort'] + (attributes['driving']*weight)*row['driving']
-    #     + (attributes['interior']*weight)*row['interior'] + (attributes['tech']*weight)*row['tech'] + (attributes['utility']*weight)*row['utility']
-
-    #     cu.append(calculated_utility)
-    # df.insert(0, 'Calculated Utility', cu, allow_duplicates=True)
-    # int(False) = 0, int(True) = 1
 def knowledge_based_rec(df,
                         manufacturer=None,
                         category=None,
@@ -39,46 +21,52 @@ def knowledge_based_rec(df,
                         tech=None,
                         utility=None):
     """
-    Calculate Utility based on Edmunds Rating based on the User's Preferences.
-    Weight is determined
+    Calculate Utility based on Edmunds Rating from the User's Requirements.
 
     """
 
-    # int(False) = 0, int(True) = 1
+    # int("0") = 0, int("1") = 1
     attributes = {'comfort': int(comfort),
                   'driving': int(driving),
                   'interior': int(interior),
                   'tech': int(tech),
-                  'utility':int(utility)}
+                  'utility':int(utility),
+                  'weight': None,
+                  'manufacturer': manufacturer,
+                  'category': category,
+                  'price': price
+                  }
 
     cu = []
 
     sum = int(comfort) + int(driving) + int(interior) + int(tech) + int(utility)
     if 0 == sum:
-        weight = 0.2
-        for i, row in df.iterrows():
-            calculated_utility = weight*row['comfort'] + weight*row['driving']
-            + weight*row['interior'] + weight*row['tech'] + weight*row['utility']
+        weight = 0
+        attributes['weight'] = weight
 
+        for i, row in df.iterrows():
+            calculated_utility = (row['comfort'] + row['driving'] + row['interior'] + row['tech'] + row['utility'])/5
             cu.append(calculated_utility)
+
     else:
         weight = 1/sum
+        attributes['weight'] = weight
+
         for i, row in df.iterrows():
             calculated_utility = (attributes['comfort']*weight)*row['comfort']
             + (attributes['driving']*weight)*row['driving'] + (attributes['interior']*weight)*row['interior'] + (attributes['tech']*weight)*row['tech'] + (attributes['utility']*weight)*row['utility']
 
             cu.append(calculated_utility)
 
-    print(f"Weight: {weight}")
-    print(cu)
     if 'Calculated Utility' in df.columns:
         del df['Calculated Utility']
 
-    df.insert(0, 'Calculated Utility', cu, allow_duplicates=True)
+    df.insert(0, 'Calculated Utility', cu, allow=_duplicatesTrue)
 
+
+    print(f"Weight: {weight}")
+    print(f"attributes: {attributes}")
     return attributes
-
-
 
 def recommend(request):
     # Get path and Read CSV FIle with weight of each vehicles
@@ -87,8 +75,8 @@ def recommend(request):
     DATA_SRC = os.path.join(cwd, 'rec', csv_name)
     df = pd.read_csv(DATA_SRC)
 
-    manufacturer = request.POST['manufacturer']
-    veh_type = request.POST['vehicle_type']
+    manufacturer = request.POST.get('manufacturer', None)
+    veh_type = request.POST.get('vehicle_type', None)
 
     price = request.POST.get('price', None)
 
@@ -98,14 +86,13 @@ def recommend(request):
     tech = request.POST.get('tech', 0)
     utility = request.POST.get('utility', 0)
 
-    # knowledge_based_rec(df, manufacturer, veh_type, None, .14, .23, .21, .12, .30)
-    attributes = knowledge_based_rec(df, manufacturer, veh_type, None, comfort, driving, interior, tech, utility)
-
-    # z = df.sort_values('Calculated Utility').tail(10)
-    z = df.reindex(index=df.index[::-1])
+    attributes = knowledge_based_rec(df, manufacturer, veh_type, price, comfort, driving, interior, tech, utility)
     z = df.sort_values('Calculated Utility').tail(10)
 
-    # data.reindex(index=data.index[::-1])
+    # if 0 != attributes['weight']:
+    #     z = df.sort_values('Calculated Utility').tail(10)
+    # else:
+    #     z = df.sort_values('overall rating').tail(10)
 
 
 
